8265f4e9ee1c91d70b2e3bf8b0542c08f42a13b6
michaelangel007 <michaelangel007@sharedcraft.com>
2025-02-12 11:23:58 -0800

    Cleanup delay()

--- source/chess.c
+++ source/chess.c
@@ -348,16 +348,18 @@ void showcase_board(char old_row, char old_col, char new_row, char new_col, int 
         // wprintf(L"Magnus Carlsen -\n");
     }
 }
+
 void delay(int number_of_seconds)
 {
     // Converting time into milli_seconds
     int milli_seconds = 1000 * number_of_seconds;
- 
-    // Storing start time
+
     clock_t start_time = clock();
- 
+    clock_t end_time   = start_time + milli_seconds;
+
     // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds);
+    while (clock() < end_time)
+        ; // intentional busy-waiting
 }
 
 void append_possible_moves()
